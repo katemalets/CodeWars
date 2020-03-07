de8a9307eb87bbadd9c134adab1f697d349adf37
katemalets <katyamask@gmail.com>
2020-03-07 21:40:45 +0300

    nums

---
+++ RomanNumeralDecoder/RomanToNumber.java
@@ -0,0 +1,43 @@
+package code.wars.java.RomanNumeralDecoder;
+
+public class RomanToNumber {
+    int value(char r) {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+
+    int romanToDecimal(String romanNumber) {
+        int digitNumber = 0;
+
+        for (int i = 0; i < romanNumber.length(); i++)
+        {
+            int s1 = value(romanNumber.charAt(i));
+            if (i + 1 < romanNumber.length()) {
+                int s2 = value(romanNumber.charAt(i + 1));
+                if (s1 >= s2) {
+                    digitNumber = digitNumber + s1;
+                } else {
+                    digitNumber = digitNumber + s2 - s1;
+                    i++;
+                }
+            } else {
+                digitNumber = digitNumber + s1;
+                i++;
+            }
+        }
+        return digitNumber;
+    }
+}
---
+++ RomanNumeralDecoder/RomanToNumberMain.java
@@ -0,0 +1,8 @@
+package code.wars.java.RomanNumeralDecoder;
+
+public class RomanToNumberMain {
+    public static void main(String args[]) {
+        RomanToNumber ob = new RomanToNumber();
+        System.out.println("Year : " + ob.romanToDecimal("IX"));
+    }
+}
