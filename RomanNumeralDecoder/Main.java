9832d42e19151ac0e45a14737f58cf7ffcb6c16a
katemalets <katyamask@gmail.com>
2020-03-07 16:23:11 +0300

    test

--- RomanNumeralDecoder/Main.java
+++
@@ -1,8 +0,0 @@
-package code.wars.java.RomanNumeralDecoder;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ RomanNumeralDecoder/RomanNumeralDecoderMain.java
@@ -0,0 +1,10 @@
+package code.wars.java.RomanNumeralDecoder;
+
+public class RomanNumeralDecoderMain {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        solution.findSolution("XXI");
+    }
+}
