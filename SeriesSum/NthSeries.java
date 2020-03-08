7fe3fae4c8f227db5b2298499214d4cea381d5cb
katemalets <katyamask@gmail.com>
2020-03-08 20:25:11 +0300

    Nth sum

---
+++ SeriesSum/NthSeries.java
@@ -0,0 +1,20 @@
+package code.wars.java.SeriesSum;
+
+public class NthSeries {
+    public static void main(String[] args) {
+        System.out.println(seriesSum(5));
+    }
+
+    public static String seriesSum(int n){
+        double sum = 1.00;
+        double three = 3.00;
+        double one = 1.00;
+        for (int i = 0; i < n; i++) {
+            sum = sum + 1 / (three * i + one);
+        }
+        sum = sum - 1;
+        sum = Math.round(sum * 100)/100.0d;
+        String result = Double.toString(sum);
+        return result;
+    }
+}
