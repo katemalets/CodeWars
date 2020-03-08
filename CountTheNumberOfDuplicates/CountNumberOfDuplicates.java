1358b0eab0fda764cffee2861341283f18a6d965
katemalets <katyamask@gmail.com>
2020-03-08 11:03:29 +0300

    counter

--- CountTheNumberOfDuplicates/CountNumberOfDuplicates.java
+++ CountTheNumberOfDuplicates/CountNumberOfDuplicates.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CountNumberOfDuplicates {
     public static void main(String[] args) {
-        String string = "abacada";
+        String string = "abaccbcada";
         char[] elements = string.toCharArray();
         Arrays.sort(elements);
         System.out.println(elements);
