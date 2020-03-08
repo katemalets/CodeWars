ffdd239f1cd3e254f093bbef2f766d542d80f819
katemalets <katyamask@gmail.com>
2020-03-08 20:51:18 +0300

    nth sum

---
+++ TakeATenMinuteWalk/TenMinWalk.java
@@ -0,0 +1,20 @@
+package code.wars.java.TakeATenMinuteWalk;
+
+public class TenMinWalk {
+    public static void main(String[] args) {
+        System.out.println(isValid(new char[] {'n','k','n','s','n','s','n','s','n','s'}));
+    }
+
+    public static boolean isValid(char[] walk) {
+        int result = walk.length;
+        if (result != 10){
+            return false;
+        }
+        for (int i = 1; i < result; i++) {
+            if (walk[i] == walk[i - 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
