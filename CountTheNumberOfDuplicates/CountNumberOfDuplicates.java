01905f069922588878529e5c02d1ecd51895c960
katemalets <katyamask@gmail.com>
2020-03-08 10:10:39 +0300

    counter

--- CountTheNumberOfDuplicates/CountNumberOfDuplicates.java
+++ CountTheNumberOfDuplicates/CountNumberOfDuplicates.java
@@ -2,16 +2,17 @@ package code.wars.java.CountTheNumberOfDuplicates;
 
 public class CountNumberOfDuplicates {
     public static void main(String[] args) {
-        String string = "ILoooveyou";
+        String string = "abcda";
         int counter = 0;
         for (int i = 0; i < string.length(); i++) {
             char firstElement = string.charAt(i);
             System.out.print(firstElement + " ");
-            for (int j = 0; j < string.length(); j++) {
+            for (int j = 1; j < string.length(); j++) {
                 char secondElement = string.charAt(j);
-               // if (String.valueOf(firstElement).compareToIgnoreCase(String.valueOf(secondElement))){
-                //    counter++;
-                //}
+               // String.valueOf(firstElement).compareToIgnoreCase(String.valueOf(secondElement));
+                if(Character.toLowerCase(firstElement) == Character.toLowerCase(secondElement)){
+                    counter++;
+                }
             }
         }
         System.out.println("\nThe number of repetitions : " + counter);
