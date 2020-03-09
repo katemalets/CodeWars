3c3df383d8a6ae1be0d963a717f17807c344e0f2
katemalets <katyamask@gmail.com>
2020-03-09 21:56:41 +0300

    hide card

--- CreditCardMask/CreditCardMask.java
+++ CreditCardMask/CreditCardMask.java
@@ -2,10 +2,11 @@ package code.wars.java.CreditCardMask;
 
 public class CreditCardMask {
     public static void main(String[] args) {
-        System.out.println(maskify("Nananananananananananananananana Batman!"));
+        System.out.println(maskify("4556364607935616"));
     }
-
+    
     public static String maskify(String string) {
+        //throw new UnsupportedOperationException("Unimplemented");
         String fourNums = "";
         String nums = "";
         String[] numbers = string.split("",string.length() - 3);
