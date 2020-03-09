dca4948ad585d6582448e0806ddde85565a62e6b
katemalets <katyamask@gmail.com>
2020-03-09 21:44:59 +0300

    hide card

---
+++ CreditCardMask/CreditCardMask.java
@@ -0,0 +1,21 @@
+package code.wars.java.CreditCardMask;
+
+public class CreditCardMask {
+    public static void main(String[] args) {
+        System.out.println(maskify("Nananananananananananananananana Batman!"));
+    }
+
+    public static String maskify(String string) {
+        String fourNums = "";
+        String nums = "";
+        String[] numbers = string.split("",string.length() - 3);
+        for (String number : numbers) {
+            if (number.length() == 4){
+                fourNums = number;
+            } else {
+                nums = nums + number;
+            }
+        }
+        return string.replaceAll(".","#") + fourNums;
+    }
+}
